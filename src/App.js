20911e12b637b46ae2aca07270556c4ad8c32246
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-09-01 00:07:03 +0300

    create project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from './components/Container';
+import StopWatch from './components/StopWatch';
+
+export default function App() {
+  return (
+    <div>
+      <Container>
+        <StopWatch />
+      </Container>
+    </div>
+  );
+}
